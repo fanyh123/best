7b5dfe2af643e18c909940b8abe3593cba88330d
ranjy <jingyingran@163.com>
2017-07-16 20:40:43 +0800

    泛型修饰基类

--- Test/src/main/java/com/ran/test/dao/mapper/BaseDao.java
+++ Test/src/main/java/com/ran/test/dao/mapper/BaseDao.java
@@ -1,16 +1,18 @@
 package com.ran.test.dao.mapper;
 
+import java.util.List;
 import java.util.Map;
 
-public interface BaseDao {
+public interface BaseDao<T> {	
 	
-	
-	public <T> void insert(T t);
+	public void insert(T t);
 	
 	public void delete(Map<String,Object> params);
 	
 	public void update(Map<String,Object> params);
 	
-	public <T> T get(Map<String,Object> params);	
+	public T get(Map<String,Object> params);
+	
+	public List<T> getAll();
 
 }
